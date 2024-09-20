a5df06f8297dcac445d6579fac316f18da7c37b7
Arash Karampour <karampour.arash@yahoo.com>
2024-09-20 17:30:22 +0200

    adding enum

--- dist/index.js
+++ dist/index.js
@@ -148,3 +148,54 @@
 //     width: 34,
 //     color: 'red'
 // }
+// Enums:
+//An enum is a special "class" that represents a group of constants (unchangeable variables).
+//Enums come in two flavors string and numeric. Lets start with numeric.
+//Numeric Enums - Default
+//By default, enums will initialize the first value to 0 and add 1 to each additional value:
+// enum CardinalDirections {
+//     North,
+//     East,
+//     South,
+//     West
+// }
+// console.log(CardinalDirections.North);
+// console.log(CardinalDirections.West);
+// let currentDirection : CardinalDirections = CardinalDirections.North;
+// currentDirection = 4 // just 0,1,2,3 is acceptable else we'll get an error
+//Numeric Enums - Initialized
+//You can set the value of the first numeric enum and have it auto increment from that:
+// enum CardinalDirections {
+//     Norht = 1,
+//     East,
+//     South,
+//     West
+// }
+// console.log(CardinalDirections.West);
+// Numeric Enums - Fully Initialized
+// You can assign unique number values for each enum value. Then the values will not incremented automatically:
+// enum StatusCodes {
+//     NotFound = 404,
+//     Success = 200,
+//     Accepted = 202,
+//     BadRequest = 400
+// }
+// console.log(StatusCodes.NotFound);
+// String Enums
+// Enums can also contain strings. This is more common than numeric enums, because of their readability and intent.
+// enum CardinalDirections {
+//     North = 'North',
+//     East = 'East',
+//     South = 'South',
+//     West = 'West'
+// }
+// console.log(CardinalDirections.North);
+// console.log(CardinalDirections.West);
+// Technically, you can mix and match string and numeric enum values, but it is recommended not to do so.
+// enum MyMixedEnum {
+//     Name = 'Arash',
+//     Age = 25,
+//     Type = 'Alpha'
+// }
+// console.log(MyMixedEnum.Name);
+// console.log(MyMixedEnum.Age);
